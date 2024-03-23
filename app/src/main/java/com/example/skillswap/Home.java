8bb9631bae3821dcf35d5e0d09892e3a4b4d5475
Admin <davitsukiasyan91@gmail.com>
2024-03-23 11:14:25 -0700

    commit

---
+++ app/src/main/java/com/example/skillswap/Home.java
@@ -0,0 +1,28 @@
+package com.example.skillswap;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import com.lorentzos.flingswipe.SwipeFlingAdapterView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Home extends Fragment {
+
+    private ArrayAdapter<String> arrayAdapter;
+    List<String> data;
+    SwipeFlingAdapterView flingAdapterView;
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_home, container, false);
+
+    }
+}
---
+++ app/src/main/java/com/example/skillswap/SkillSwap.java
@@ -0,0 +1,105 @@
+package com.example.skillswap;
+
+import static com.example.skillswap.R.id.home;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.skillswap.databinding.ActivitySkillswapBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class SkillSwap extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
+    TextView textView;
+    Home home= new Home();
+    Chats settings = new Chats();
+    Profile profile = new Profile();
+    FirebaseAuth auth;
+    ImageButton imageButton, imageButton1;
+    FirebaseUser user;
+
+    ActivitySkillswapBinding binding;
+
+    @SuppressLint({"NonConstantResourceId", "MissingInflatedId"})
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_skillswap);
+        getSupportActionBar().hide();
+
+
+        auth  = FirebaseAuth.getInstance();
+        imageButton =(ImageButton) findViewById(R.id.settings);
+        imageButton1 = (ImageButton) findViewById(R.id.logout);
+        imageButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(SkillSwap.this, "kpar vopshem", Toast.LENGTH_SHORT).show();
+                 FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getApplicationContext(), Login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(SkillSwap.this, "mi hate kapr", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(SkillSwap.this, Settings.class);
+                startActivity(intent);
+
+            }
+        });
+        user = auth.getCurrentUser();
+        if (user == null){
+            Intent intent = new Intent(getApplicationContext(), Login.class);
+            startActivity(intent);
+            finish();
+
+        }
+
+        binding = ActivitySkillswapBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new Home());
+        binding.bottomNavigation.setOnItemSelectedListener(menuItem -> {
+            int itemId = menuItem.getItemId();
+
+            if (itemId == R.id.home){
+                replaceFragment(new Home());
+            }
+            else if (itemId == R.id.profile){
+                replaceFragment(new Profile());
+            }
+            else if (itemId == R.id.chats){
+                replaceFragment(new Chats());
+            }
+            return true;
+        });
+
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout,fragment);
+        fragmentTransaction.commit();
+    }
+
+}
